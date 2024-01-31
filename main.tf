6944103e98dccf9d0ae4f8a4f1051cb4de92e5bd
Thapathisri <138443548+Thapathisri@users.noreply.github.com>
2024-01-31 14:05:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-2"
 }
-
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
-  acl    = var.bucket_acl
-  versioning {
-    enabled = var.versioning_enabled
+ 
+}
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = var.versioning_enabled
   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
 
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
