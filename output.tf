cf5722fe2f382452e2b81224ca836ae59e9d9161
Thapathisri <138443548+Thapathisri@users.noreply.github.com>
2024-01-31 14:06:33 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,4 @@
 output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.bucket.id
+  description = "The ID of the created S3 bucket."
+  value       = aws_s3_bucket.terraform_state.id
 }
-
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.bucket.arn
-}
-
